e84f64ab32c66544eaa56e8ddc15a586b8b4525b
Eric Brawer <ericbrawer@gmail.com>
2013-09-16 22:50:18 -0400

    Created a user page/authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Jungle::Application.routes.draw do
+  # get "users/create"
+  resources :users, :only => [:new, :create]
   resources :products
-  get "products/index"
-  get "products/show"
-  get "products/new"
-  get "products/edit"
+  # get "products/index"
+  # get "products/show"
+  # get "products/new"
+  # get "products/edit"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
